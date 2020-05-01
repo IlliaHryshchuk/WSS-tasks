498811a9800a0b02564638ed8dc2313fbae68806
IlliaHryshchuk <45259941+IlliaHryshchuk@users.noreply.github.com>
2020-05-01 19:47:34 +0300

    update snail algorithm
    - all snail methods were moved into separate class;
    - added length validator;
    - added a pause in the end of program;

--- WSS_first_task/Program.cs
+++ WSS_first_task/Program.cs
@@ -9,82 +9,21 @@ namespace WSS_first_task
 {
     class Program
     {
-        delegate int Action(ref int[,] arr, int i, int j, int num);
-
-
-        static void printMatrix(int[,] arr)
-        {
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(0); j++)
-                {
-                    Console.Write(arr[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-        }
-
-
-        static int fillMatrix(ref int[,] arr, int i, int j, int num)
-        {
-            arr[i,j] = num;
-            num++;
-            return num;
-        }
-
-
-        static int printSnailedMatrix(ref int[,] arr, int i, int j, int num)
-        {
-            Console.Write(arr[i, j] + " ");
-            return 1;
-        }
-
-
-        static int[,] snailAlgorithm(int[,] arr, Action func)
-        {
-            int j = 0, num = 0;
-            for (int i = 1; i <= arr.GetLength(0) / 2; i++)
-            {
-                for (j = i - 1; j < arr.GetLength(0) - i + 1; j++)
-                    num = func(ref arr, i - 1, j, num);
-                
-                for (j = i; j < arr.GetLength(0) - i + 1; j++)
-                    num = func(ref arr, j, arr.GetLength(0) - i, num);
-                
-                for (j = arr.GetLength(0) - i - 1; j >= i - 1; --j) 
-                    num = func(ref arr, arr.GetLength(0) - i, j, num);
-                
-                for (j = arr.GetLength(0) - i - 1; j >= i; j--)
-                    num = func(ref arr, j, i - 1, num);
-            }
-            if (arr.GetLength(0) % 2 == 1)
-                num = func(ref arr, arr.GetLength(0) / 2, arr.GetLength(0) / 2, num);
-
-            return arr;
-        }
-
-
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a size of matrix: ");
-            int sizeOfArr = int.Parse(Console.ReadLine());
+            int sizeOfArr = SnailMatrix.setValidLength();
             int[,] arr = new int[sizeOfArr, sizeOfArr];
-            Action action;
 
-
-            action = fillMatrix;
-            arr = snailAlgorithm(arr, action);
+            arr = SnailMatrix.snailAlgorithm(arr, SnailMatrix.fillMatrix);
             Console.WriteLine("\nMatrix: ");
-            printMatrix(arr);
-
+            SnailMatrix.printMatrix(arr);
 
             Console.WriteLine("\n\nReading a matrix in <snail> way: ");
-            action = printSnailedMatrix;
-            arr = snailAlgorithm(arr, action);
+            arr = SnailMatrix.snailAlgorithm(arr, SnailMatrix.printSnailedMatrix);
 
-
-            Console.WriteLine();
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey(true);
+            return;
         }
     }
 }
---
+++ WSS_first_task/SnailMatrix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSS_first_task
+{
+    class SnailMatrix
+    {
+        public delegate int Action(ref int[,] arr, int i, int j, int num);
+
+
+        public static int setValidLength()
+        {
+            int sizeOfArr;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a size of matrix: ");
+
+                if (!int.TryParse(Console.ReadLine(), out sizeOfArr) || sizeOfArr <= 0)
+                    Console.WriteLine("Input error! Try again.");
+                else
+                    return sizeOfArr;
+            }
+        }
+
+
+        public static void printMatrix(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        public static int fillMatrix(ref int[,] arr, int i, int j, int num)
+        {
+            arr[i, j] = num;
+            num++;
+            return num;
+        }
+
+
+        public static int printSnailedMatrix(ref int[,] arr, int i, int j, int num)
+        {
+            Console.Write(arr[i, j] + " ");
+            return 1;
+        }
+
+
+        public static int[,] snailAlgorithm(int[,] arr, Action func)
+        {
+            int j = 0, num = 0;
+            for (int i = 1; i <= arr.GetLength(0) / 2; i++)
+            {
+                for (j = i - 1; j < arr.GetLength(0) - i + 1; j++)
+                    num = func(ref arr, i - 1, j, num);
+
+                for (j = i; j < arr.GetLength(0) - i + 1; j++)
+                    num = func(ref arr, j, arr.GetLength(0) - i, num);
+
+                for (j = arr.GetLength(0) - i - 1; j >= i - 1; --j)
+                    num = func(ref arr, arr.GetLength(0) - i, j, num);
+
+                for (j = arr.GetLength(0) - i - 1; j >= i; j--)
+                    num = func(ref arr, j, i - 1, num);
+            }
+            if (arr.GetLength(0) % 2 == 1)
+                num = func(ref arr, arr.GetLength(0) / 2, arr.GetLength(0) / 2, num);
+
+            return arr;
+        }
+    }
+}
